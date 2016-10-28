25be82ae3a62f702a7c6bdfe594605aed3aff2d7
got2be07 <got2be07@hotmail.fr>
2016-10-28 13:43:54 +0200

    default val

--- script/interface.php
+++ script/interface.php
@@ -5,6 +5,6 @@ $bpm = $_REQUEST['bpm'];
 $color = $_REQUEST['color'];
 
 $db = new PDO("mysql:host=localhost;dbname=bpm_title;charset=UTF8", 'root', '');
-$sql = 'REPLACE INTO bpm_title(title, bpm, color) VALUES("'.$title.'", '.$bpm.', "'.$color.'")';
+$sql = 'REPLACE INTO bpm_title(title, bpm, color) VALUES("'.$title.'", '.(empty($bpm) ? 0 : $bpm).', "'.$color.'")';
 $resql = $db->query($sql);
 echo $sql;exit;
