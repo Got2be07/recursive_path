fdf2ad27fba267e33587d3edc45ddf2645c610a2
Gauthier VERDOL <got2be07@hotmail.fr>
2019-01-01 22:12:56 +0100

    NEW : plus de replace, mais update si existant

--- script/interface.php
+++ script/interface.php
@@ -5,6 +5,17 @@ $bpm = $_REQUEST['bpm'];
 $color = $_REQUEST['color'];
 
 $db = new PDO("mysql:host=localhost;dbname=bpm_title;charset=UTF8", 'root', '');
-$sql = 'REPLACE INTO bpm_title(title, bpm, color) VALUES("'.utf8_encode($title).'", '.(empty($bpm) ? 0 : $bpm).', "'.$color.'")';
+
+$sql = 'SELECT rowid FROM bpm_title WHERE title = "'.utf8_encode($title).'"';
 $resql = $db->query($sql);
-echo $sql;exit;
+$res = $resql->fetchAll();
+if(!empty($res)) $id = (int)$res[0]['rowid'];
+else $id = 0;
+
+if(!empty($id)) { // Ligne déjà existante
+	$sql_update = 'UPDATE bpm_title SET title = "'.utf8_encode($title).'", bpm = '.(empty($bpm) ? 0 : $bpm).', color = "'.$color.'" WHERE rowid = '.$id;
+	$resql = $db->query($sql_update);
+} else { // Nouvelle ligne
+	$sql_add = 'INSERT INTO bpm_title(title, bpm, color) VALUES("'.utf8_encode($title).'", '.(empty($bpm) ? 0 : $bpm).', "'.$color.'")';
+	$resql = $db->query($sql_add);
+}
